dc184d60049465ff15643355d5e3e56a4d32533a
Taylor Lohman <taylorlohman@Taylors-MacBook-Pro.local>
2020-01-06 12:00:55 -0800

    installed typescript and deleted my signup outline

--- foodie-fun-app/src/components/Authentication/Signup.js
+++ foodie-fun-app/src/components/Authentication/Signup.js
@@ -93,11 +93,3 @@ export const Signup = () => {
     </form>
   );
 };
-
-// return (
-//     <div>
-//       <h1>Signup</h1>
-//       <p>username/email (validations needed ($(/w+)@.(/w+)^)</p>
-//       <p>password input</p>
-//     </div>
-//   );
