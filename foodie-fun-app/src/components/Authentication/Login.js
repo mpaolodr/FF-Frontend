d9fef7838c99a5a7089bb09670d831f2b0332fa5
mpaolodr <mpaolodr_1504@yahoo.com>
2020-01-06 11:30:38 -0500

    added yup and react hook form as dependencies

--- foodie-fun-app/src/components/Authentication/Login.js
+++ foodie-fun-app/src/components/Authentication/Login.js
@@ -1,11 +1,57 @@
 import React from "react";
 
 export const Login = () => {
-	return (
-		<div>
-		<h1>Login</h1>
-		<p>username/email (validations needed ($(/w+)@.(/w+)^)</p>
-		<p>password input</p>
-		</div>
-	);
+  return (
+    <div>
+      <h1>Login</h1>
+      <p>username/email (validations needed ($(/w+)@.(/w+)^)</p>
+      <p>password input</p>
+    </div>
+  );
 };
+
+// const initialFormState = { email: "", password: "" };
+// const [userInfo, setUserInfo] = useState(initialFormState);
+
+// //input change handler
+// const changeHandler = e => {
+//   const { name, value } = e.target;
+//   setUserInfo({
+//     ...userInfo,
+//     [name]: value
+//   });
+// };
+
+// //submit function
+// const submit = e => {
+//   e.preventDefault();
+//   //axios post request
+//   //if userInfo exists, continue to homepage, if not throw an error
+//   console.log("submitted");
+//   setUserInfo(initialFormState);
+// };
+
+//   <form onSubmit={submit}>
+//       <h2>Login</h2>
+//       <div>
+//         <label htmlFor="email">Email</label>
+//         <input
+//           type="email"
+//           name="email"
+//           id="email"
+//           value={userInfo.email}
+//           onChange={changeHandler}
+//         />
+//       </div>
+//       <div>
+//         <label htmlFor="password">Password</label>
+//         <input
+//           type="password"
+//           name="password"
+//           id="password"
+//           value={userInfo.password}
+//           onChange={changeHandler}
+//         />
+//       </div>
+//       <button type="submit">Login</button>
+//     </form>
--- foodie-fun-app/src/components/Authentication/Signup.js
+++ foodie-fun-app/src/components/Authentication/Signup.js
@@ -1,11 +1,13 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
 
-export const Signup = (props) => {
-	return (
-		<div>
-		<h1>Signup</h1>
-		<p>username/email (validations needed ($(/w+)@.(/w+)^)</p>
-		<p>password input</p>
-		</div>
-	);
-}
+export const Signup = () => {
+  return (
+    <div>
+      <h1>Signup</h1>
+      <p>username/email (validations needed ($(/w+)@.(/w+)^)</p>
+      <p>password input</p>
+    </div>
+  );
+};
