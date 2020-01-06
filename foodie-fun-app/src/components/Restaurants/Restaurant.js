0410f8438161edf32f2b536d1bb39062ed791014
Taylor Lohman <taylorlohman@Taylors-MacBook-Pro.local>
2020-01-06 12:48:57 -0800

    added fake review data and modified restaurant detail page and restaurants page (layouts)

--- foodie-fun-app/src/components/Restaurants/Restaurant.js
+++ foodie-fun-app/src/components/Restaurants/Restaurant.js
@@ -1,14 +1,25 @@
 import React from "react";
 
+// MARK: -- Third Party Packages
+import { Button, Container, Row, Col } from "reactstrap";
+import { Link } from "react-router-dom";
+
 export const Restaurant = (props) => {
 	return (
-		<div>
+		<Container>
 			<h1>Restaurant Detail</h1>
-			<p>Add Review</p>
+			<Row>
+			<Col lg={{ offset: 9 }} md={{ offset: 6 }} xs={{ offset: 3 }}>
+			<Link to="/edit-restaurant"><Button color="" outline><i class="fas fa-edit"></i> restaurant</Button></Link>
+			<Link to="/add-review"><Button color="success" outline><i class="fas fa-plus"></i> review</Button></Link>
+			</Col>
+			<Col lg={{ offset: 11 }} md={{ offset: 8 }} xs={{ offset: 5 }}>
+				<p>Rating</p>
+			</Col>
+			</Row>
 			<p>Edit Restaurant Detail</p>
 			<p>overall rating</p>
 			<p>all reviews</p>
-			<p>photos</p>
-		</div>
+		</Container>
 	);
 };
