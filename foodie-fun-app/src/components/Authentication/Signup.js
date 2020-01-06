ecc678dfda1ad6c205a23f5bec4f1e11e5c1fb80
mpaolodr <mpaolodr_1504@yahoo.com>
2020-01-06 18:33:36 -0500

    finished styling sign up form for desktop version

--- foodie-fun-app/src/components/Authentication/Signup.js
+++ foodie-fun-app/src/components/Authentication/Signup.js
@@ -1,9 +1,111 @@
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
 import * as yup from "yup";
+import styled from "styled-components";
+import backgroundImg from "./assets/signup-bg-2.svg";
 
 //loader component
 import Loader from "./Loader";
+
+//styled componentns
+const FormContainer = styled.div`
+  background: url(${backgroundImg}) no-repeat center;
+  background-size: cover;
+  width: 100%;
+  height: 100vh;
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  mix-blend-mode: multiply;
+
+  &:after {
+    content: "";
+    background: rgba(0, 0, 0, 0.6);
+    height: 100%;
+    width: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: -1;
+  }
+`;
+
+const Heading = styled.h1`
+  color: #ecb847;
+  font-size: 2.5rem;
+  margin: 10px 0;
+`;
+
+const Form = styled.form`
+  border-radius: 10px;
+  background: rgba(24, 18, 18, 0.8);
+  width: 30%;
+  height: 70%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const IndField = styled.div`
+  width: 80%;
+  height: 90px;
+  display: flex;
+  justify-items: space-between;
+  align-items: flex-start;
+  flex-direction: column;
+  position: relative;
+`;
+
+const Label = styled.label`
+  font-size: 0.9rem;
+  color: #ecb847;
+`;
+
+const InputField = styled.input`
+  width: 100%;
+  padding: 5px 10px;
+  border-radius: 5px;
+`;
+
+const ErrorMessage = styled.p`
+  font-size: 0.8rem;
+  position: absolute;
+  bottom: -20px;
+  left: 0;
+  color: #fff;
+
+  &:before {
+    content: "⚠ ";
+    display: inline;
+  }
+`;
+
+const SubmitBtn = styled.button`
+  width: 30%;
+  padding: 5px;
+  color: #fff;
+  font-weight: 500;
+  font-size: 1rem;
+  background: #c45228;
+  border: 0.5px solid #c45228;
+  border-radius: 10px;
+  transition: all 0.5s;
+
+  &:hover {
+    background: #fff;
+    color: #c45228;
+    transition: all 0.5s;
+  }
+`;
+
+const BottomText = styled.p`
+  color: white;
+  font-size: 0.8rem;
+`;
 
 export const Signup = () => {
   //loader animation
@@ -74,39 +176,60 @@ export const Signup = () => {
   };
 
   return (
-    <div className="form-container">
+    <FormContainer className="form-container">
       {loaderState.loading ? (
         <Loader />
       ) : (
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <h1>Sign-Up</h1>
-          <div className="ind-field">
-            <label htmlFor="email">Email</label>
-            <input name="email" type="email" ref={register} />
-            {errors.email && <p>{errors.email.message}</p>}
-          </div>
+        <Form onSubmit={handleSubmit(onSubmit)}>
+          <Heading>Sign-Up</Heading>
+          <IndField className="ind-field">
+            <Label htmlFor="email">Email</Label>
+            <InputField name="email" type="email" ref={register} />
+            {errors.email && (
+              <ErrorMessage>{errors.email.message}</ErrorMessage>
+            )}
+          </IndField>
 
-          <div className="ind-field">
-            <label htmlFor="username">Username</label>
-            <input type="text" name="username" ref={register} />
-            {errors.username && <p>{errors.username.message}</p>}
-          </div>
+          <IndField className="ind-field">
+            <Label htmlFor="username">Username</Label>
+            <InputField type="text" name="username" ref={register} />
+            {errors.username && (
+              <ErrorMessage>{errors.username.message}</ErrorMessage>
+            )}
+          </IndField>
 
-          <div className="ind-field">
-            <label htmlFor="password">Password</label>
-            <input type="password" name="password" ref={register} />
-            {errors.password && <p>{errors.password.message}</p>}
-          </div>
+          <IndField className="ind-field">
+            <Label htmlFor="password">Password</Label>
+            <InputField type="password" name="password" ref={register} />
+            {errors.password && (
+              <ErrorMessage>{errors.password.message}</ErrorMessage>
+            )}
+          </IndField>
 
-          <div className="ind-field">
-            <label htmlFor="confirmPassword">Confirm Password</label>
-            <input type="password" name="confirmPassword" ref={register} />
-            {errors.confirmPassword && <p>{errors.confirmPassword.message}</p>}
-          </div>
+          <IndField className="ind-field">
+            <Label htmlFor="confirmPassword">Confirm Password</Label>
+            <InputField type="password" name="confirmPassword" ref={register} />
+            {errors.confirmPassword && (
+              <ErrorMessage>{errors.confirmPassword.message}</ErrorMessage>
+            )}
+          </IndField>
 
-          <button type="submit">Submit</button>
-        </form>
+          <SubmitBtn type="submit">Submit</SubmitBtn>
+          <BottomText>
+            Already have an account?{" "}
+            <Link
+              style={{
+                color: "#ecb847",
+                fontSize: "0.8rem"
+              }}
+              to="/login"
+            >
+              Sign in
+            </Link>{" "}
+            now!
+          </BottomText>
+        </Form>
       )}
-    </div>
+    </FormContainer>
   );
 };
