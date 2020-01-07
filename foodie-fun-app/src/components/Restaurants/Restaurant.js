381148cad41919cd7d0d9a88e89f78bb0f3c29db
Taylor Lohman <taylorlohman@Taylors-MacBook-Pro.local>
2020-01-06 19:22:55 -0800

    added review to restaurant details. Added reviews to review (review will be overall)

--- foodie-fun-app/src/components/Restaurants/Restaurant.js
+++ foodie-fun-app/src/components/Restaurants/Restaurant.js
@@ -1,9 +1,11 @@
 import React from "react";
 
+// Mark: -- Component
+import { Reviews } from "../Reviews";
+
 // MARK: -- Third Party Packages
-import { Button, Container, Row, Col, Table, Card, CardBody } from "reactstrap";
+import { Button, Container, Row, Col } from "reactstrap";
 import { Link } from "react-router-dom";
-import { ReactStars } from "../Reviews";
 
 export const Restaurant = (props) => {
 	return (
@@ -15,57 +17,7 @@ export const Restaurant = (props) => {
 				<Link to="/add-review"><Button color="success" outline><i class="fas fa-plus"></i> review</Button></Link>
 			</Col>
 			</Row>
-			<Row>
-				<Col>
-					<h2>
-						Reviews
-					</h2>
-				</Col>
-				<Col>
-					<h5><i class="fas fa-star text-warning"></i> 3.00 | 20 reviews</h5>
-				</Col>			
-			</Row>
-			<Card xs={{ size: 10, offset: 2 }}>
-			<Row>
-			<Col xs={{ size: 12 }} md={{ size: 6 }}>
-				<Table>
-					<tbody>
-						<tr>
-							<th scope="row">Food</th>
-							<td><ReactStars count={5} size={24}/></td>
-						</tr>
-						<tr>
-							<th scope="row">Drinks</th>
-							<td><ReactStars count={5} size={24}/></td>
-						</tr>
-						<tr>
-							<th scope="row">Service</th>
-							<td><ReactStars count={5} size={24}/></td>
-						</tr>
-					</tbody>
-				</Table>
-			</Col>
-			<Col xs={{ size: 12 }} md={{ size: 6 }}>
-				<Table>
-					<tbody>
-						<tr>
-							<th scope="row">Decor</th>
-							<td><ReactStars count={5} size={24}/></td>
-						</tr>
-						<tr>
-							<th scope="row">Cleaniness</th>
-							<td><ReactStars count={5} size={24}/></td>
-						</tr>
-						<tr>
-							<th scope="row">Vibe</th>
-							<td><ReactStars count={5} size={24}/></td>
-						</tr>
-					</tbody>
-				</Table>
-			</Col>
-			</Row>
-			</Card>
-			<p>all reviews</p>
+			<Reviews />
 		</Container>
 	);
 };
--- foodie-fun-app/src/components/Reviews/Review.js
+++ foodie-fun-app/src/components/Reviews/Review.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { Container, Row, Col, Card, Media } from "reactstrap";
 
 export const Review = () => {
 	return (
-		<h1>Review Detail</h1>
+		<Container>
+			<Card>
+				<Media>
+					<Media left href="#">
+						<Media object data-src="holder.js/64x64" alt="Generic placeholder image" />
+					</Media>
+					<Media body>
+						This is where the written review will be. This is where the written review will be.
+					</Media>
+				</Media>
+			</Card>
+		</Container>
 	);
 };
