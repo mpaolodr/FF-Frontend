cef244e330dab8f9cd8185dc2ab9b6c9b634f19f
Taylor Lohman <taylorlohman@Taylors-MacBook-Pro.local>
2020-01-07 10:03:18 -0800

    changed ReviewPage to ReviewTable with ReviewTableCell. Modified Reviews to have ReviewTable. Modified Profile to hold ReviewTable

--- foodie-fun-app/src/components/Reviews/index.js
+++ foodie-fun-app/src/components/Reviews/index.js
@@ -1,5 +1,6 @@
 export { Reviews } from "./Reviews";
-export { Review } from "./Review";
+export { ReviewTable } from "./ReviewTable";
+export { ReviewTableCell } from "./ReviewTableCell";
 export { EditReview } from "./EditReview";
 export { AddReview } from "./AddReview";
 export { ReactStars } from "./Rating";
