c5117f244d993c8551b1d468aa53c4073d4a7716
Taylor Lohman <taylorlohman@Taylors-MacBook-Pro.local>
2020-01-06 17:50:57 -0800

    added to detail page and navigation

--- foodie-fun-app/src/components/Restaurants/Restaurant.js
+++ foodie-fun-app/src/components/Restaurants/Restaurant.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 // MARK: -- Third Party Packages
-import { Button, Container, Row, Col, Table } from "reactstrap";
+import { Button, Container, Row, Col, Table, Card, CardBody } from "reactstrap";
 import { Link } from "react-router-dom";
 import { ReactStars } from "../Reviews";
 
@@ -11,16 +11,23 @@ export const Restaurant = (props) => {
 			<h1>Restaurant Name (Detail Page)</h1>
 			<Row>
 			<Col xs={{ size: 'auto' }}>
-			<Link to="/edit-restaurant"><Button color="" outline><i class="fas fa-edit"></i> restaurant</Button></Link>
-			<Link to="/add-review"><Button color="success" outline><i class="fas fa-plus"></i> review</Button></Link>
-			</Col>
-			<Col className="ml-auto" xs={{ size: 'auto' }}>
-			<h3><i class="fas fa-star text-warning"></i> 3.00</h3>
+				<Link to="/edit-restaurant"><Button color="" outline><i class="fas fa-edit"></i> restaurant</Button></Link>
+				<Link to="/add-review"><Button color="success" outline><i class="fas fa-plus"></i> review</Button></Link>
 			</Col>
 			</Row>
 			<Row>
-			<Col lg={{ size: 'auto' }}>
-				<Row>
+				<Col>
+					<h2>
+						Reviews
+					</h2>
+				</Col>
+				<Col>
+					<h5><i class="fas fa-star text-warning"></i> 3.00 | 20 reviews</h5>
+				</Col>			
+			</Row>
+			<Card xs={{ size: 10, offset: 2 }}>
+			<Row>
+			<Col xs={{ size: 12 }} md={{ size: 6 }}>
 				<Table>
 					<tbody>
 						<tr>
@@ -35,6 +42,12 @@ export const Restaurant = (props) => {
 							<th scope="row">Service</th>
 							<td><ReactStars count={5} size={24}/></td>
 						</tr>
+					</tbody>
+				</Table>
+			</Col>
+			<Col xs={{ size: 12 }} md={{ size: 6 }}>
+				<Table>
+					<tbody>
 						<tr>
 							<th scope="row">Decor</th>
 							<td><ReactStars count={5} size={24}/></td>
@@ -47,15 +60,11 @@ export const Restaurant = (props) => {
 							<th scope="row">Vibe</th>
 							<td><ReactStars count={5} size={24}/></td>
 						</tr>
-						<tr>
-							<th scope="row">Review</th>
-							<td><ReactStars count={5} size={24}/></td>
-						</tr>
 					</tbody>
 				</Table>
-				</Row>
 			</Col>
 			</Row>
+			</Card>
 			<p>all reviews</p>
 		</Container>
 	);
