826412ca2e613ae3937839fc0e31f79b91e262df
Taylor Lohman <taylorlohman@Taylors-MacBook-Pro.local>
2020-01-09 23:51:50 -0800

    added post and get

--- foodie-fun-app/src/reducers/index.js
+++ foodie-fun-app/src/reducers/index.js
@@ -1,8 +1,8 @@
-import { combineReducers } from "redux";
+// import { combineReducers } from "redux";
 
 import restaurantReducer from "./restaurantReducer";
-import { ratingReducer } from "./ratingReducer";
-import { reviewReducer } from "./reviewReducer";
+//import { ratingReducer } from "./ratingReducer";
+//import { reviewReducer } from "./reviewReducer";
 
 export default restaurantReducer
 
