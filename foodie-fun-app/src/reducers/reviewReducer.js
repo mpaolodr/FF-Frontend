73b5e519d318f263d27323a60b652d74a10841a3
Taylor Lohman <taylorlohman@Taylors-MacBook-Pro.local>
2020-01-12 11:46:37 -0800

    removed api key

--- foodie-fun-app/src/reducers/reviewReducer.js
+++ foodie-fun-app/src/reducers/reviewReducer.js
@@ -1,4 +1,18 @@
-import { FETCH_REVIEW_START, FETCH_REVIEW_SUCCESS, FETCH_REVIEW_FAILURE } from "../actions";
+import {
+FETCH_REVIEW_START
+FETCH_REVIEW_SUCCESS
+FETCH_REVIEW_FAILURE
+POST_REVIEW_START
+POST_REVIEW_SUCCESS
+POST_REVIEW_FAILURE
+PUT_REVIEW_START
+PUT_REVIEW_SUCCESS
+PUT_REVIEW_FAILURE
+DELETE_REVIEW_START
+DELETE_REVIEW_SUCCESS
+DELETE_REVIEW_FAILURE
+} from "../actions";
+
 
 
 const initialState = {
@@ -26,6 +40,27 @@ export const reviewReducer = (state = initialState, action) => {
 				isFetching: false,
 				error: action.payload
 			}
+		case POST_REVIEW_START:
+			return {
+				...state,
+				isFetching: true,
+			}
+		case POST_REVIEW_SUCCESS:
+			return {
+				...state,
+				isFetching: false,
+				reviews
+			}
+		case POST_REVIEW_FAILURE:
+
+
+		case PUT_REVIEW_START:
+		case PUT_REVIEW_SUCCESS:
+		case PUT_REVIEW_FAILURE:
+
+		case DELETE_REVIEW_START:
+		case DELETE_REVIEW_SUCCESS:
+		case DELETE_REVIEW_FAILURE:
 		default:
 			return state;
 	}
